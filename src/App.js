fe0c25a56b1c6325a287d7e87a0601d4c3ca7179
William Jordan <WilliamKatahdin@gmail.com>
2022-11-05 23:47:13 -0600

    port

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Counter from "./components/Counter";
+import MyCounter from "./components/MyCounter";
 import MyGitHub from "./components/MyGitHub";
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
       </h2>
 
       <hr />
-      <Counter incBy={1}/><hr />
-      <Counter incBy={2}/><hr />
+      <MyCounter incBy={1}/><hr />
+      <MyCounter incBy={2}/><hr />
       <MyGitHub /><hr />
 
     </div>
---
+++ src/components/MyCounter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+const MyCounter = () => {
+    // let countState = 77
+    const [countState, setCountState] = useState(77)
+
+    function incCount() {
+        setCountState(countState +1)
+    }
+    return (
+        <div>
+            <div>{countState}</div>
+            <button onClick={incCount}>+1</button>
+        </div>
+    )
+}
+export default MyCounter;
